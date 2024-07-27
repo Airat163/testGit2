0147b3b64c1a824083f6668fe67d8435c79ce586
Airat163 <93443042+Airat163@users.noreply.github.com>
2024-07-27 15:09:24 +0400

    renamed to Samara(origin)

--- src/main/java/org/example1/Main.java
+++ src/main/java/org/example1/Main.java
@@ -2,8 +2,9 @@ package org.example1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello Samara!");
         int x = 18;
         String s;
+        
     }
 }
